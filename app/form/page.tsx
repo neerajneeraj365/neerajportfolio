2d5a267ff09d42018ff77913183d3deb33cbc56a
Neeraj Neeraj <neerajneeraj@Neerajs-MacBook-Air.local>
2024-03-01 00:44:44 -0500

    added message in form

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -47,6 +47,7 @@ const FormPage = () => {
       </div>
       <div className="flex flex-col gap-6 md:gap-8 my-8 md:w-1/2">
         <p className="text-md">Say Hi to me...</p>
+        <p>The Form is in build...Its not working for now. Maintainence going on</p>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <FormField
