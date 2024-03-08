9442c0cb24825ce74f6606cf1d3d2a345c54243d
Neeraj Neeraj <neerajneeraj@Neerajs-MacBook-Air.local>
2024-03-07 21:55:15 -0500

    added form

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -3,46 +3,33 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import emailjs from "@emailjs/browser";
+import { useForm, ValidationError } from "@formspree/react";
+import Image from "next/image";
 
 const Form = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-
-  const [error, setError] = useState(false);
-  const [success, setSuccess] = useState(false);
-
-  const sendEmail = () => {
-    const templateParams = JSON.stringify({
-      from_name: email,
-      to_name: "Neeraj",
-      message,
-    });
-
-    emailjs
-      .sendForm(
-        process.env.NEXT_PUBLIC_SERVICE_KEY ?? "",
-        process.env.NEXT_PUBLIC_TEMPLATE_KEY ?? "",
-        templateParams,
-        {
-          publicKey: process.env.NEXT_PUBLIC_KEY,
-        }
-      )
-      .then(
-        () => {
-          console.log("SUCCESS!");
-          setName("");
-          setEmail("");
-          setMessage("");
-          setSuccess(true);
-        },
-        (error) => {
-          console.log("FAILED...", error.text);
-          setError(true);
-        }
-      );
-  };
+  const [state, handleSubmit] = useForm("meqyzgjj");
+  if (state.succeeded) {
+    return (
+      <div className="flex flex-col justify-center items-center h-full w-full">
+        <Image src="/done.png" height="400" width="400" alt="form completion" />
+        <p className="m-5 text-lg">Success!!</p>
+        <Link href="/" className="underline">
+          Back to Homepage
+        </Link>
+      </div>
+    );
+  }
+  if (state.errors) {
+    return (
+      <div className="flex flex-col justify-center items-center h-full w-full">
+        <Image src="/failed.png" height="400" width="400" alt="form failed" />
+        <p className="m-5 text-lg">Failed!!</p>
+        <Link href="/" className="underline">
+          Try again
+        </Link>
+      </div>
+    );
+  }
   return (
     <section className="flex flex-col w-full justify-center items-center">
       <div className="flex flex-col gap-6 md:gap-8 my-8 lg:w-1/2">
@@ -56,28 +43,22 @@ const Form = () => {
             nikatwork365@gmail.com
           </Link>
         </p>
-        <form onSubmit={sendEmail}>
+        <form onSubmit={handleSubmit}>
           <div className="flex flex-col w-full gap-4">
             <input
               type="text"
               name="name"
-              value={name}
               className="border-gray-200 border-b-2 h-10 bg-transparent outline-none"
               placeholder="John Smith"
-              onChange={(e: any) => setName(e.target.value)}
             />
             <input
               type="email"
               name="email"
-              value={email}
-              onChange={(e: any) => setEmail(e.target.value)}
               className="border-gray-200 border-b-2 h-10 bg-transparent outline-none"
               placeholder="xyz@gmail.com"
             />
             <textarea
               name="message"
-              value={message}
-              onChange={(e: any) => setMessage(e.target.value)}
               id="textarea"
               rows={5}
               className="border-gray-200 border-b-2 bg-transparent outline-none resize-none"
@@ -85,13 +66,11 @@ const Form = () => {
             />
             <Button
               className="flex justify-center items-center my-4"
-              onClick={sendEmail}
+              type="submit"
             >
               Submit
             </Button>
           </div>
-          {success && <span>Success</span>}
-          {error && <span>Error</span>}
         </form>
       </div>
     </section>
